c7cee3e21d8b8f41419198a70ece3c1304e74387
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-17 13:07:20 +0200

    Clearing code

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,12 +1,29 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  amount: { type: Number, required: true },
-  date: { type: Date, required: true },
-  _id: { type: String },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  description: {
+    type: String,
+    required: [true, "Amount is required"],
+  },
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+  },
+  amount: {
+    type: Number,
+    required: [true, "Amount is required"],
+  },
+  date: {
+    type: Date,
+    required: [true, "Date is required"],
+  },
+    _id: {
+        type: String
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId, ref:
+            "User"
+    },
 });
 
 module.exports = mongoose.model("Transaction", transactionSchema);
