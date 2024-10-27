7a4b9a05bc4041f9c4aca7e824dd9ccb1ef44957
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-27 12:32:25 +0100

    Update Session.js

--- models/Session.js
+++ models/Session.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const sessionSchema = new mongoose.Schema({
-  uid: { type: ObjectId },
+  uid: { type: String },
 });
 
 module.exports = mongoose.model("Session", sessionSchema);
